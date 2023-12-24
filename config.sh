962a92f887ed8c7292c3a2e292539fc68f586961
nuneRol <154561978+nuneRol@users.noreply.github.com>
2023-12-24 16:21:46 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,7 @@
 apt install curl chromium-browser npm -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-source ~/.bashrc
-nvm install 22
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && source ~/.bashrc
+nvm install 21.5.0
 npm install
 node app.js
